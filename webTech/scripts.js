5fe3b8f62bfd29fb64e85de63ed99baa07414479
Benjamin Clarke <benjaminclarke@benjamiarkesmbp.default>
2015-03-01 18:45:39 +0000

    article page working

--- webTech/scripts.js
+++ webTech/scripts.js
@@ -1,3 +1,12 @@
+"use strict";
+
+window.onload = function() {
+  start(); 
+};
+
+var STATE_DISLIKE = 0;
+var STATE_LIKE = 0;
+
 //Changes element with ID equal to s1
 function changeElement(elementID,content) {
    document.getElementById(elementID).innerHTML = content;
@@ -28,8 +37,43 @@ function hideElement(elementID)	{
 	//$('#elementID').style.display = 'none';
 }
 
+function hideClass(classID)	{
+	var divs = document.getElementsByClassName(classID);
+	for(var i = 0; i < divs.length; i = i + 1) {
+        divs[i].style.display="none";
+    }
+}
+
+function showClass(classID)	{
+	var divs = document.getElementsByClassName(classID);
+	for(var i = 0; i < divs.length; i = i + 1) {
+        divs[i].style.display="block";
+    }
+}
+
+function transitionVerticlePosition(classID, FinalHeight)	{
+	var divs = document.getElementsByClassName(classID);
+	var b = 0;
+	var opacityIncrement = (1/(FinalHeight/0.3));
+	var opacity = 0;
+	function delayedLoop () {           //  create a loop function
+		setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+			divs[0].style.top= b + "%";
+			console.log(opacity);
+			opacity = opacity + opacityIncrement;
+			divs[0].style.opacity = opacity;
+			b = b + 0.3;                    //  increment the counter
+			if (b <= FinalHeight) {            //  if the counter < 10, call the loop function
+				delayedLoop();             //  ..  again which will trigger another 
+			}                        //  ..  setTimeout()
+		}, 1)
+	}
+	divs[0].style.opacity =1;
+	delayedLoop();
+}
+
 function showElement(elementID)	{
-	document.getElementById(elementID).style.display = "inline";
+	document.getElementById(elementID).style.display = "block";
 }
 
 function draw(elementID) {
@@ -41,3 +85,81 @@ function draw(elementID) {
     // Apply JavaScript APIs for drawing
  	}
  }
+
+ function getWindowWidth() {
+    return window.outerWidth;    
+    //document.getElementById("demo").innerHTML = txt;
+}
+
+function getWindowHeight()	{
+	return window.outerHeight;
+}
+
+function checkLessThan(comparison, value)	{
+	if(value <= comparison)	{
+		return true;
+	} else	{
+		return false;
+	}
+}
+
+function changeNavigation()	{
+	if(checkLessThan(740,getWindowWidth()))	{
+		hideElement('wideNavBar');
+		showElement('cssmenu');
+	} else	{
+		showElement('wideNavBar');
+		hideElement('cssmenu');
+	}
+}
+
+function start()	{
+	var i = document.getElementById("dislike");
+    i.addEventListener('click', function(e) {
+        e.preventDefault();
+		ToggleDislike();
+     });
+
+    document.getElementById("like").addEventListener('click',function(e)	{
+        e.preventDefault();
+		ToggleLike();
+    });
+}
+
+function swap(element,image)	{
+    element.style.backgroundImage = "url("+image+ ")";
+}
+
+
+function ToggleLike() {
+  var el = document.getElementById('like');
+  var dl = document.getElementById('dislike');
+	el.classList.toggle('likeClicked');
+
+	if(dl.classList.contains('dislikeClicked'))	{
+  		dl.classList.toggle('dislikeClicked')
+ 	}
+}
+
+function ToggleDislike() {
+  var el = document.getElementById('dislike');
+  var l = document.getElementById('like');
+
+	el.classList.toggle('dislikeClicked');
+
+	if(l.classList.contains('likeClicked'))	{
+		l.classList.toggle('likeClicked');
+  	}
+}
+
+// document.getElementById('toggler').addEventListener('click', function() {
+//   colorToggle();
+// });
+
+
+
+
+
+
+
+
